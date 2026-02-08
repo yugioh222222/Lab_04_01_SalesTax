38b46c5b7583d991a8568ca198efa0cec5feda8f
caleb <wambsgcc@mail.uc.edu>
2026-02-07 20:24:30 -0500

    commit final

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -1,6 +1,15 @@
 public class SalesTax
 {
     static void main(String[] args) {
+        double purchasePrice = 21.00;
+        final double TAX_RATE = 1.05;
+        double totalPrice = purchasePrice * TAX_RATE;
+
+        System.out.println(" Your total before taxes is " + purchasePrice + " and after taxes it will be " + totalPrice);
+
+
+
+
 
     }
 
